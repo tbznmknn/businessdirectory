27389463e4074c27968bd2b9db54313b47349d30
Radnaabazar Bulgan <radnaa2015@gmail.com>
2025-10-09 11:52:43 +0800

    eslint ignore generator

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ export default [
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
   {
-    ignores: ['**/dist'],
+    ignores: ['**/dist', '**/generated', '**/node_modules', '**/.nx'],
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
--- libs/database/eslint.config.mjs
+++ libs/database/eslint.config.mjs
@@ -17,6 +17,6 @@ export default [
     },
   },
   {
-    ignores: ['**/out-tsc'],
+    ignores: ['**/out-tsc', '**/generated/**', 'src/generated/**'],
   },
 ];
